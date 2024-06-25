7ad27c8b3401c1e5a42e4c31b3b6a3b10fe41cd9
Candrasa Asmaradanta <155295765+Asmara9@users.noreply.github.com>
2024-06-25 07:43:45 +0700

    Delete asmara-resto-apps directory

--- asmara-resto-apps/src/scripts/globals/api-endpoint.js
+++
@@ -1,8 +0,0 @@
-import CONFIG from './config';
-
-const API_ENDPOINT = {
-  LIST_RESTAURANTS: 'https://restaurant-api.dicoding.dev/list',
-  DETAIL_RESTAURANT: (id) => `${CONFIG.BASE_URL}/detail/${id}`,
-};
-
-export default API_ENDPOINT;
